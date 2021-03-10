a9e4c5c94474035dd848337a03b7729239a7dfbe
Robert Buckley <rob.buckley93@gmail.com>
2021-03-10 17:17:02 -0600

    ninja homework

---
+++ ninja/ninja.js
@@ -0,0 +1,22 @@
+class ninja{
+    constructor(ninjaName, ninjaHealth){
+        this.name = ninjaName,
+        this.health = ninjaHealth,
+        this.speed = 3,
+        this.strength = 3
+    }
+    sayName(name){
+        console.log(this.name);
+    }
+    showStats(health,speed,strength){
+        console.log( `${this.name} has ${this.health} health, ${this.speed} speed, and ${this.strength} strength`);
+    }
+    drinkSake(health){
+        this.health += 10;
+    }
+}
+const ninja1 = new ninja("Hyabusa", 10);
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
+ninja1.showStats();
