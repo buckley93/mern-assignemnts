321af4bb2d0a033bb77436a4d52c709618c533d3
Robert Buckley <rob.buckley93@gmail.com>
2021-03-15 18:51:58 -0500

    turning in first jsx homework

---
+++ first-jsx/src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello Dojo!</h1>
+      </header>
+      <h3>Things I need to do</h3>
+      <table>
+        <ul>
+          <li>Learn React</li>
+          <li>Climb Mt.Everest</li>
+          <li>Run a marathon</li>
+          <li>Feed the girlfriend before she gets hangry</li>
+        </ul>
+      </table>
+    </div>
+  );
+}
+
+export default App;
